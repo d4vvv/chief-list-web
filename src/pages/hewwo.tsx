b70ab648c3acfc9863a03e23af50b26fd0813e67
Kacper <kacper.godula@schibsted.com>
2023-05-04 22:54:40 +0200

    add basic auth

---
+++ src/pages/hewwo.tsx
@@ -0,0 +1,45 @@
+import { User, createClient } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { Button } from '../../components/ui/button'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY ?? ''
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default function Home() {
+  const router = useRouter()
+  const [activeUser, setActiveUser] = useState<User | null>(null)
+
+  const getUser = async () => {
+    {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+
+      if (user) {
+        setActiveUser(user)
+      }
+    }
+  }
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (!error) {
+      console.log('signed out boi')
+      router.push('/')
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  return (
+    <main>
+      <p>Hewwo: </p>
+      <p className=''>{activeUser?.email}</p>
+      <Button onClick={signOut}>Log out</Button>
+    </main>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from 'react'
-import { MainPage } from '../../components/MainPage'
-import { Container } from '../../components/ui/Container'
-import { Sidebar } from '../../components/ui/Sidebar'
 import { createClient } from '@supabase/supabase-js'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Button } from '../../components/ui/button'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ''
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY ?? ''
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default function Home() {
+  const router = useRouter()
   const [users, setUsers] = useState<any[] | undefined>([])
+  const [activeUser, setActiveUser] = useState()
   const testFunction = async () => {
     let { data, error } = await supabase.from('testTable').select('name')
 
@@ -18,16 +19,88 @@ export default function Home() {
     setUsers(users)
   }
 
+  const signUp = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email: 'r3pcak99@gmail.com',
+      password: 'plzwork',
+    })
+
+    console.log({ data })
+    console.log({ error })
+  }
+
+  const getSessionData = async () => {
+    const { data, error } = await supabase.auth.getSession()
+    console.log(data)
+  }
+
+  const signIn = async () => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: 'r3pcak99@gmail.com',
+      password: 'plzwork',
+    })
+
+    console.log({ data })
+    console.log({ error })
+
+    if (data.session) {
+      supabase.auth.setSession({
+        access_token: data.session.access_token,
+        refresh_token: data.session.refresh_token,
+      })
+    }
+
+    if (!error) {
+      router.push('/hewwo')
+    }
+  }
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+    if (!error) {
+      console.log('signed out boi')
+    }
+  }
+
+  const getUser = async () => {
+    {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser()
+      if (user) {
+        router.push('/hewwo')
+      }
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  const getActiveUser = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
+    console.log(user)
+  }
+
   useEffect(() => {
     testFunction()
+    // signUp()
+    // getActiveUser()
   }, [])
 
   return (
     <main>
-      <Container>
+      <div className=''>
+        <Button onClick={signUp}>signUp</Button>
+        <Button onClick={signIn}>signIn</Button>
+        <Button onClick={signOut}>signOut</Button>
+      </div>
+      {/* <Container>
         <Sidebar />
         <MainPage users={users} />
-      </Container>
+      </Container> */}
     </main>
   )
 }
